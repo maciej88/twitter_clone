428bdc6de27ab3f2bcb10fe94d38fb4a542b22c4
Maciej Krywult <mkrywult88@gmail.com>
2020-12-31 12:32:49 +0100

    starting write models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Tweet(models.Model):
+    content = models.CharField(max_length=200)
+    creation_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 
+
+
 class Base(View):
     def get(self, request):
         return render(request, 'base.html')
