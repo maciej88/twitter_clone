cf5b03945f3cde3b9c65ff18e196bb32a72eac9c
Maciej Krywult <mkrywult88@gmail.com>
2020-12-31 12:44:13 +0100

    user login as email config

--- clone/settings.py
+++ clone/settings.py
@@ -84,6 +84,11 @@ DATABASES = {
     }
 }
 
+# User substitution
+# https://docs.djangoproject.com/en/1.11/topics/auth/customizing/#auth-custom-user
+
+AUTH_USER_MODEL = 'app.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
